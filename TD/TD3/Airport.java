d0f8078402856f3fa1c633d6d50eae7f8ec6dd1a
cneron89 <clementnerondat@gmail.com>
2016-02-09 23:15:26 +0100

    Ajout Airport.java

---
+++ TD/TD3/Airport.java
@@ -0,0 +1,24 @@
+public class Airport {
+	private int idAirport;
+    private String country;
+    private String city;
+    
+	public int getIdAirport() {
+		return idAirport;
+	}
+	public void setIdAirport(int idAirport) {
+		this.idAirport = idAirport;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+}
