e2e07b3c63c3f1f729bd827373ebdd4995b297a5
cneron89 <clementnerondat@gmail.com>
2016-03-14 09:14:20 +0100

    ajout de getFlighDescription.js et update de flightDescription.html

---
+++ Preplane/src/main/webapp/getFlightDescription.js
@@ -0,0 +1,27 @@
+function getFlightDescription(){
+  var dep = document.getElementById("departure").value
+  var arriv = document.getElementById("arrival").value
+  var dh =  document.getElementById("dh").value
+  var ah =  document.getElementById("ah").value
+  var comno = document.getElementById("comno").value
+  var atc = document.getElementById("atc").value
+  var crew = document.getElementById("crew").value
+  var status = document.getElementById("status").value
+
+  $.ajax({
+    url: "http://localhost:8080/ws/preplane/flight_a?departure="+dep+
+    "&arrival="+arriv+
+    "&ddate="+dh+
+    "&comno="+comno+
+    "&atc="+atc+
+    "&status="+status+
+    "&crew="+crew+
+    "&arrivdate="+ah,
+    dataType: "json",
+    Type : "GET"
+  })
+  .done(function( data ) { console.log( "Got data", data );
+    alert("Flight added successfully")
+    window.location = "http://localhost:8080/flightDescription.html";
+  });
+}
