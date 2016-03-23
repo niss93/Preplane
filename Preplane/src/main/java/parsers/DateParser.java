81979517330ab96f140597dad88bb4b64e10fd5b
lsaffa78 <leasaffar@hotmail.fr>
2016-03-23 10:39:42 +0100

    Modif parser, wsuser, occ

--- Preplane/src/main/java/parsers/DateParser.java
+++ Preplane/src/main/java/parsers/DateParser.java
@@ -4,10 +4,12 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.eclipse.jetty.util.log.Log;
+
 public class DateParser {
 	public static Date StringToDate(String date) {
 		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-
+		
 		try {
 
 			Date newdate = formatter.parse(date);
